efc4b44f25d542ccf15fe105fc19bcdeb498460a
Girmay <ink423103@yahoo.com>
2019-11-10 15:09:14 +0100

    goodBy added

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,9 @@ const helloWorld = function() {
   return 'Hello world!';
 };
 
+const gooodbyWorld = function () {
+  return 'Good Bye World!';
+}
 console.log(helloWorld());
 
 console.log("Almost there!");
